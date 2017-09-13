8937150bc7dee7d8aebfb3f022593d467421f4ec
Chris Donnelly <chris_j_donnelly@hotmail.com>
2017-09-14 00:34:28 +0100

    country info is populating and deleting on new selection

--- client/src/app.js
+++ client/src/app.js
@@ -8,6 +8,27 @@ function app() {
     var countriesView = new CountriesView();
     
     restCountries.get(countriesView.populate);
+
+    var select = document.getElementById('country-drop-down');
+    
+    select.addEventListener('change', function() {
+        console.log(restCountries.data);
+        var selectedCountry = this.value;
+        var ul = document.getElementById('country-data');
+        while(ul.firstChild) {
+            ul.removeChild(ul.firstChild);
+          }
+        var li = document.createElement('li');
+        li.innerText = 'Country: ' + restCountries.data[selectedCountry].name;
+        ul.appendChild(li);
+        var li = document.createElement('li');
+        li.innerText = 'Capital: ' + restCountries.data[selectedCountry].capital;
+        ul.appendChild(li);
+        var li = document.createElement('li');
+        var li = document.createElement('li');
+        li.innerText = 'Region: ' + restCountries.data[selectedCountry].region;
+        ul.appendChild(li);
+    })
  
 }
 
